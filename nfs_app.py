6b2e7f596b93a4e049777df98997b45a9d12d904
Tom Kamphuys <tomkamphuys@hotmail.com>
2024-08-17 22:53:07 +0200

    first try at gui using Remi

--- nfs_app.py
+++ nfs_app.py
@@ -189,4 +189,4 @@ if __name__ == "__main__":
     # starts the webserver
     # optional parameters
     # start(MyApp,address='127.0.0.1', port=8081, multiple_instance=False,enable_file_cache=True, update_interval=0.1, start_browser=True)
-    start(NFSApp, debug=True, address='0.0.0.0', port=8081, start_browser=True, multiple_instance=True)
+    start(NFSApp, debug=True, address='0.0.0.0', port=8082, start_browser=True, multiple_instance=True)
